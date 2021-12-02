5f7a68d7f4b72ffc577a752e84d18f9ea921113d
dohuuhung211 <68835298+dohuuhung211@users.noreply.github.com>
2021-12-02 17:40:49 +0700

    hung commit

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\ProductTag;
+use App\Models\Tag;
 use App\Traits\StorageImageTrait;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use App\Components\Recusive;
 use App\Models\Category;
@@ -13,15 +18,22 @@ class AdminProductController extends Controller
     use StorageImageTrait;
     private $category;
     private $product;
-    public function __construct(Category $category, Product $product)
+    private $productImage;
+    private $tag;
+    private $productTag;
+    public function __construct(Category $category, Product $product, ProductImage $productImage, Tag $tag, ProductTag $productTag)
     {
         $this->category = $category;
         $this->product = $product;
+        $this->productImage = $productImage;
+        $this->tag = $tag;
+        $this->productTag = $productTag;
 
     }
 
     public function index(){
-        return view('admin.product.index');
+        $products = $this->product->latest()->paginate(5);
+        return view('admin.product.index', compact('products'));
     }
     public function getCategory($parentId){
         $data = $this->category->all();
@@ -34,20 +46,52 @@ class AdminProductController extends Controller
         return view('admin.product.add', compact('htmlOption'));
     }
     public function store(Request $request){
-        $dataProductCreate = [
-            'name' => $request->name,
-            'price' => $request->price,
-            'content' => $request->contents,
-            'user_id' => auth()->id(),
-            'category_id' => $request->category_id
-        ];
-        $dataUploadFeatureImage = $this->storageTraitUpload($request, 'feature_image_path', 'products');
-        if (!empty($dataUploadFeatureImage)){
-            $dataProductCreate['feature_image_name'] = $dataUploadFeatureImage['file_name'];
-            $dataProductCreate['feature_image_path'] = $dataUploadFeatureImage['file_path'];
-        }
+        try {
+            DB::beginTransaction();
+            $dataProductCreate = [
+                'name' => $request->name,
+                'price' => $request->price,
+                'content' => $request->contents,
+                'user_id' => auth()->id(),
+                'category_id' => $request->category_id
+            ];
+            $dataUploadFeatureImage = $this->storageTraitUpload($request, 'feature_image_path', 'products');
+            if (!empty($dataUploadFeatureImage)){
+                $dataProductCreate['feature_image_name'] = $dataUploadFeatureImage['file_name'];
+                $dataProductCreate['feature_image_path'] = $dataUploadFeatureImage['file_path'];
+            }
 
-        $product = $this->product->create($dataProductCreate);
-        dd($product);
+            $product = $this->product->create($dataProductCreate);
+            // them data vao product_images
+            if ($request->hasFile('image_path')){
+                foreach ($request->image_path as $fileItem){
+                    $dataProductImageDetail = $this->storageTraitUploadMultiple($fileItem, 'products');
+                    $product->images()->create([
+                        'image_path' => $dataProductImageDetail['file_path'],
+                        'image_name' => $dataProductImageDetail['file_name']
+                    ]);
+                }
+            }
+
+            // them tags cho product
+            if (!empty($request->tags)){
+                foreach ($request->tags as $tagItem){
+                    $tagInstance = $this->tag->firstOrCreate(['name' => $tagItem]);
+                    $tagIds[] = $tagInstance->id;
+                }
+            }
+
+            $product->tags()->attach($tagIds);
+            DB::commit();
+            return redirect()->route('products.index');
+        } catch (\Exception $exception){
+            DB::rollBack();
+            Log::error('Message'. $exception->getMessage() . '--- At Line: ' . $exception->getLine());
+        }
+    }
+    public function edit($id){
+        $product = $this->product->find($id);
+        $htmlOption = $this->getCategory($product->category_id);
+        return view('admin.product.edit', compact('product', 'htmlOption'));
     }
 }
